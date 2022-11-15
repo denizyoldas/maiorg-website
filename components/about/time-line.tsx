3f2f682942c817b95ee38e60bea4562cf55ede6a
denizyoldas <daksu30@gmail.com>
2022-11-15 23:55:29 +0300

    blog layout

---
+++ components/about/time-line.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const TimeLine = () => {
+  return <div>demo</div>
+}
+
+export default TimeLine
--- pages/about.tsx
+++ pages/about.tsx
@@ -1,8 +1,21 @@
+import Head from 'next/head'
 import React from 'react'
 import MainLayout from '../components/layout/main-layout'
 
 const AboutPage = () => {
-  return <MainLayout>demo</MainLayout>
+  return (
+    <MainLayout>
+      <Head>
+        <title>Mai Organizasyon - Hakkımızda</title>
+
+        <meta
+          name="description"
+          content="Mai Organizasyon olarak, siz değerli müşterilerimizin her türlü organizasyon ihtiyacını karşılamak için çalışıyoruz. Sizlerin mutlu olması bizim için en büyük ödül."
+        />
+      </Head>
+      Devam
+    </MainLayout>
+  )
 }
 
 export default AboutPage
